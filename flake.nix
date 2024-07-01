82574c15dedc39ced0579eef16e3d536b2db8ead
polygon <polygon@wh2.tu-dresden.de>
2024-07-01 19:04:53 +0200

    Create Nix recipe, usable state

--- flake.nix
+++ flake.nix
@@ -2,32 +2,47 @@
   inputs = {
     nixpkgs.url = "github:/NixOS/nixpkgs/nixos-24.05";
     rust-overlay.url = "github:oxalica/rust-overlay";
+    naersk = {
+      url = "github:nix-community/naersk";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, rust-overlay }:
+  outputs = { self, nixpkgs, rust-overlay, naersk }:
     let
       system = "x86_64-linux";
       overlays = [ (import rust-overlay) ];
       pkgs = import nixpkgs { inherit overlays system; };
-      python_env =
-        pkgs.python3.withPackages (ps: with ps; [ python-rtmidi mido ipython ]);
+      rust-bin = (pkgs.rust-bin.stable.latest.default.override {
+        extensions = [ "rust-src" ];
+        targets = [ "wasm32-unknown-unknown" ];
+      });
+      naersk-lib = naersk.lib.${system}.override {
+        cargo = rust-bin;
+        rustc = rust-bin;
+      };
+      dev-deps = with pkgs; [
+        rust-analyzer
+        rustfmt
+        lldb
+        cargo-geiger
+        wireshark
+        cargo-flamegraph
+      ];
+      build-deps = with pkgs; [ pkg-config ];
+      runtime-deps = with pkgs; [ rtmidi alsa-lib.dev ];
     in {
       devShells.${system}.default = pkgs.mkShell {
-        buildInputs = with pkgs; [
-          (rust-bin.stable.latest.default.override {
-            extensions = [ "rust-src" ];
-            targets = [ "wasm32-unknown-unknown" ];
-          })
-          rust-analyzer
-          rustfmt
-          wireshark
-          python_env
-          rtmidi
-          pkg-config
-          alsa-lib.dev
-          cargo-flamegraph
-        ];
+        buildInputs = dev-deps ++ build-deps ++ runtime-deps;
         LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [ pkgs.alsa-lib ];
       };
+
+      packages.${system}.midimaxe = naersk-lib.buildPackage {
+        pname = "midimaxe";
+        root = ./.;
+        buildInputs = runtime-deps;
+        nativeBuildInputs = build-deps;
+      };
+      defaultPackage.${system} = self.packages.${system}.midimaxe;
     };
 }
