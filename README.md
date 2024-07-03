89a7128adf33e1a28c6c3e50ae3416ae74a55fbf
polygon <polygon@wh2.tu-dresden.de>
2024-07-03 23:45:13 +0200

    Testing asciinema

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Midimaxe
 
+[![asciicast](https://asciinema.org/a/EvLaL88JvkaVTNHCR4e7L5J2I.svg)](https://asciinema.org/a/EvLaL88JvkaVTNHCR4e7L5J2I)
--- flake.nix
+++ flake.nix
@@ -22,6 +22,7 @@
         rustc = rust-bin;
       };
       dev-deps = with pkgs; [
+        rust-bin
         rust-analyzer
         rustfmt
         lldb
